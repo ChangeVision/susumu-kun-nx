0ef13170b82600d8121932dfed5b98d41e52f3ad
Hiroki Kondo <hiroki.kondo@change-vision.com>
2015-03-25 18:56:31 +0900

    Use Chrome Identify API

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+console.log('open popup');
+
+chrome.identity.launchWebAuthFlow({
+  'url':'https://github.com/login/oauth/authorize?client_id=eae84108cca55862ee54',
+  'interactive':true
+},
+  function(redirect_url){
+    console.log(redirect_url);
+  })
