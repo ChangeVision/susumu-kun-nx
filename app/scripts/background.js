0238210009645155df8704810dafacafd513734b
Hiroki Kondo <hiroki.kondo@change-vision.com>
2015-03-26 19:44:07 +0900

    Add global variables and setup GitHub API

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,6 +4,19 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+this.issueUrlRegExp = new RegExp(
+        'https:\/\/github.com\/(.+)\/(.+)\/(issues|pull)\/(.+)');
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+this.stateLabels = [ 'doing', 'accepting', 'reopen', 'done' ];
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.url.match(issueUrlRegExp)) {
+    chrome.pageAction.show(tabId);
+  }
+});
+
+this.GitHub = new GitHub(GitHubOptions);
+var authorized = function(data){
+  GitHub.accessToken = data.access_token;
+};
+GitHub.authorizeChromeApp().then(authorized);
